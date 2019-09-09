14cee30b45918b77afc847fd9b493acf0528b660
Nagarajuvandali <vandalinagaraj@gmail.com>
2019-09-09 20:50:18 +0530

    Add files via upload
    This program will be explaining about the Polymorphism. In the program we have 2 different classes but with same methods. So we can call the particular method by creating instance of the particular class.

---
+++ Polymorphism.py
@@ -0,0 +1,18 @@
+class Hyderabad:
+    def traffic(self):
+        print("less pollution")
+        print("free roads")
+        print("less crowdy")
+
+class Delhi:
+    def traffic(self):
+        print("Heavy pollution")
+        print("Many AC Buses")
+        print("More cars")
+
+
+travelling1 = Hyderabad()
+travelling1.traffic()
+
+travelling2 = Delhi()
+travelling2.traffic()
